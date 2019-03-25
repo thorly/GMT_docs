59121fc9511b5edb4ac7b12f378caadcc5063ecb
TravisCI <travis@nothing.com>
2019-03-25 23:02:12 +0000

    Deploy 6.0.0 from TravisCI

--- 6.0.0/_downloads/81d6fb7d347342978dfbbad3fa8f6748/map-gmtplot-0.sh
+++ 6.0.0/_downloads/81d6fb7d347342978dfbbad3fa8f6748/map-gmtplot-0.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
-J=X1.8i
-R=0/5/0/5
-X_offset=2.5i
+gmt begin map_frame_type pdf,png
+gmt set FONT_TITLE 18p
 
-gmt begin frame_type pdf,png
-gmt set FONT_TITLE 15p
+gmt subplot begin 1x5 -Fs4c/4c -M0.5c/0.2c
 
-gmt set MAP_FRAME_TYPE plain
-gmt basemap -J$J -R$R -Bx1+l"plain" -By1 -BWSen
+gmt basemap -JM -R0/10/0/10 -Baf -B+t"fancy" -c1,1 --MAP_FRAME_TYPE=fancy
 
-gmt set MAP_FRAME_TYPE inside
-gmt basemap -J$J -R$R -Bx1 -By1 -BWSen -X$X_offset
-gmt text -J$J -R$R -F+f16p -N  << EOF
-2.5 -1.2 inside
+gmt basemap -Baf -B+t"fancy\053" -c1,2 --MAP_FRAME_TYPE=fancy+
+
+gmt basemap -JX -R0/10/0/10 -Bxaf -BWSen+t"plain" -c1,3 --MAP_FRAME_TYPE=plain
+
+gmt basemap -Baf -BWSen -c1,4 --MAP_FRAME_TYPE=inside
+gmt text -F+f18p -N << EOF
+5 12 inside
 EOF
 
-gmt set MAP_FRAME_TYPE graph
-gmt basemap -J$J -R$R -Bx1+l"graph" -By1 -BWS -X$X_offset
+gmt basemap -Baf -BWS+t"graph" -c1,5 --MAP_FRAME_TYPE=graph
 
-J=M1.8i
-
-gmt set MAP_FRAME_TYPE fancy
-gmt basemap -J$J -R$R -B1 -X-3.75i -Y2.8i
-gmt text -J$J -R$R -F+f16p -N  << EOF
-2.5 -1.2 fancy
-EOF
-
-gmt set MAP_FRAME_TYPE fancy+
-gmt basemap -J$J -R$R -B1 -X2.5i
-gmt text -J$J -R$R -F+f16p -N  << EOF
-2.5 -1.2 fancy+
-EOF
+gmt subplot end
 gmt end
