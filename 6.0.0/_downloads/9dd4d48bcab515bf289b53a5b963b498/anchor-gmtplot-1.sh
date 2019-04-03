f2079ce76a9771cf9e369e4ec34370c298df043c
TravisCI <travis@nothing.com>
2019-04-03 19:56:41 +0000

    Deploy 6.0.0 from TravisCI

--- 6.0.0/_downloads/9dd4d48bcab515bf289b53a5b963b498/anchor-gmtplot-1.sh
+++ 6.0.0/_downloads/9dd4d48bcab515bf289b53a5b963b498/anchor-gmtplot-1.sh
@@ -1,14 +1,26 @@
-#!/bin/bash
-gmt begin text_justification pdf,png
-gmt text -R0/10/0/2 -Jx2c -B1g1 -BWS -F+j+f20p,blue << EOF
-1 1 LT LT
-2 1 LM LM
-3 1 LB LB
-4 1 CT CT
-5 1 CM CM
-6 1 CB CB
-7 1 RT RT
-8 1 RM RM
-9 1 RB RB
+gmt begin anchor-2 png,pdf
+gmt basemap -Rg -JH10c -B0
+gmt text -F+f15p,1,red+c+j -Dj0.5c/0.5c+v2p -N << EOF
+TL TL TL
+TC TC TC
+TR TR TR
+ML ML ML
+MC BL MC
+MR MR MR
+BL BL BL
+BC BC BC
+BR BR BR
+EOF
+
+gmt plot -R0/10/0/6 -JX10c/5c -Sc0.3c -Gred -N << EOF
+0 0
+0 3
+0 6
+5 0
+5 3
+5 6
+10 0
+10 3
+10 6
 EOF
 gmt end
