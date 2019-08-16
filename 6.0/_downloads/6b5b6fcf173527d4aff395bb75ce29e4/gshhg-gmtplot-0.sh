55ded793235b477ddae36a9feb8d3790406cf279
TravisCI <travis@nothing.com>
2019-08-16 01:02:11 +0000

    Deploy 6.0 from TravisCI

--- 6.0/_downloads/6b5b6fcf173527d4aff395bb75ce29e4/gshhg-gmtplot-0.sh
+++ 6.0/_downloads/6b5b6fcf173527d4aff395bb75ce29e4/gshhg-gmtplot-0.sh
@@ -1,8 +1,10 @@
 gmt begin map png,pdf
-gmt set MAP_TITLE_OFFSET -10p
-gmt coast -JM4c -R-158.3/-157.6/21.2/21.8 -B+t"-Df" -W1p -Df
-gmt coast -JM4c -R-158.3/-157.6/21.2/21.8 -B+t"-Dh" -W1p -Dh -X4c
-gmt coast -JM4c -R-158.3/-157.6/21.2/21.8 -B+t"-Di" -W1p -Di -X4c
-gmt coast -JM4c -R-158.3/-157.6/21.2/21.8 -B+t"-Dl" -W1p -Dl -X4c
-gmt coast -JM4c -R-158.3/-157.6/21.2/21.8 -B+t"-Dc" -W1p -Dc -X4c
+gmt set MAP_TITLE_OFFSET -15p FONT_TITLE 15p,Courier-Bold
+gmt subplot begin 1x5 -Fs4c -JM4c -R-158.3/-157.6/21.2/21.8 -B+n -M0
+gmt coast -B+t"-Df" -W1p -Df -c
+gmt coast -B+t"-Dh" -W1p -Dh -c
+gmt coast -B+t"-Di" -W1p -Di -c
+gmt coast -B+t"-Dl" -W1p -Dl -c
+gmt coast -B+t"-Dc" -W1p -Dc -c
+gmt subplot end
 gmt end
