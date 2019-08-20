91ffb8b0ad5fb883b89a4f788368f8263d7ec1c0
TravisCI <travis@nothing.com>
2019-08-20 03:34:16 +0000

    Deploy 6.0 from TravisCI

--- 6.0/_downloads/469a7e5b61cd7171e70622da05d223c1/vector-gmtplot-1.sh
+++ 6.0/_downloads/469a7e5b61cd7171e70622da05d223c1/vector-gmtplot-1.sh
@@ -1,6 +1,6 @@
 gmt begin vector-head pdf,png
 for symbol in t c a A i I; do
-    echo 1 1 0 3 | gmt plot -R0/5/0/2 -JX2c/1c -Sv0.5c+b${symbol} -W1.5p -Gred -X2c
-    echo 3 0.5 $symbol | gmt text -F+f15p,9 -N
+    echo 1 1 0 3 | gmt plot -R0/5/0/2 -JX2c/1c -Sv0.5c+b${symbol} -W1.5p -Gred -X2.2c
+    echo 3 0.5 -Sv0.5c+b$symbol | gmt text -F+f7p,9 -N
 done
 gmt end
