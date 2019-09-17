2dcf3953fe773e96ebd28b6575f833f7ea2e9bd2
TravisCI <travis@nothing.com>
2019-09-17 01:38:41 +0000

    Deploy 6.0 from TravisCI

--- 6.0/_downloads/13c5b89907694106908cd5c5aca35f4e/subplot-gmtplot-0.sh
+++ 6.0/_downloads/13c5b89907694106908cd5c5aca35f4e/subplot-gmtplot-0.sh
@@ -9,4 +9,4 @@ gmt subplot begin ${nrow}x${ncol} -Fs5c/3c+d -Blrtb
     echo 0.5 0.5 '@;red;'$i,$j'@;;' '(@;blue;'$index'@;;)' | gmt text -R0/1/0/1 -F+f20p -c
     done
 gmt subplot end
-gmt end
+gmt end show
--- 6.0/_downloads/e1a810492fb52b45ec677c1f4ba731bc/commands-gmtplot-0.sh
+++ 6.0/_downloads/e1a810492fb52b45ec677c1f4ba731bc/commands-gmtplot-0.sh
@@ -1,3 +1,3 @@
 gmt begin GlobalMap png,pdf
     gmt coast -Rg -JH15c -Gpurple -Baf -B+t"My First Plot"
-gmt end
+gmt end show
